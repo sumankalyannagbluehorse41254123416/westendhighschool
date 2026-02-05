8764318c2cb6848f7ccc15d7086765fa5bd3c9b6
santana <santanarana3@gmail.com>
2026-02-05 17:12:09 +0530

    comment

---
+++ components/about-us/school-anthem/NationalAnthemSection.tsx
@@ -0,0 +1,71 @@
+const NationalAnthemSection = () => {
+  return (
+    <div className="inner_con">
+      <p></p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Jana-Gana-Mana-Adhinayaka Jaya He,
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Bharata-Bhagya-Vidhata
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Punjab-Sindhu-Gujarata-Maratha Dravida-Utkala-Banga
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Vindhya-Himachala-Yamuna-Ganga Uchchhala-JaladthaTaranga
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Tava Subha Name Jage
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Tava Subha Ashisa Mage
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Gahe Tava Jaya Gatha.
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Jana-Gana-Mangala Dayaka&nbsp;Jaya He
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Bharata-Bhagya-Vidhata,
+        </span>
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        <span style={{ fontFamily: "comic sans ms, sans-serif", fontSize: "large" }}>
+          Jaya He, Jaya He, Jaya He, Jaya Jaya Jaya, Jaya He
+        </span>
+      </p>
+
+      <p></p>
+    </div>
+  );
+};
+
+export default NationalAnthemSection;
