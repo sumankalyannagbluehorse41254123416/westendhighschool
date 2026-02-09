c10fde5e69caed44e4e74bd24555adaf64a6e1a4
santana <santanarana3@gmail.com>
2026-02-09 18:49:24 +0530

    change

--- app/about-us/principal-message/page.tsx
+++ app/about-us/principal-message/page.tsx
@@ -1,8 +1,11 @@
+import InnerContent from "@/components/about-us/about-mission/InnerContent";
 import PrincipalBanner from "@/components/about-us/principal-message/PrincipalBanner";
+
 
 export default function PrincipalMessage() {
   return <>
   <PrincipalBanner />
+  <InnerContent />
   </>
   ;
 }
