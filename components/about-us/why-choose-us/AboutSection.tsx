ffe0ee7e171afb108471c99f4f5eb4f3ae42d857
santana <santanarana3@gmail.com>
2026-02-09 16:48:34 +0530

    change

---
+++ components/about-us/why-choose-us/AboutSection.tsx
@@ -0,0 +1,41 @@
+export default function AboutSection() {
+  return (
+    <div className="inner_con">
+      <p></p>
+
+      <p style={{ textAlign: "justify" }}>
+        <span
+          style={{
+            fontSize: "large",
+            fontFamily: "book antiqua, palatino",
+          }}
+        >
+          Our Institution is an integral part of the West End High School and our
+          association with education dates back to 1949, West End High School
+          Society has been a non profit, non proprietary organization striving
+          for excellence in education. The aim of West End High School Society
+          has always been magnanimous and this has been proved by the fact that
+          schools have been set up in the remotest corners of the country,
+          enabling the schools to impart the values to all willing learners
+          nationwide
+        </span>
+      </p>
+
+      <p style={{ textAlign: "justify" }}>
+        <span
+          style={{
+            fontSize: "large",
+            fontFamily: "book antiqua, palatino",
+          }}
+        >
+          The school has almost become a miniature India, for students come here
+          for various parts of the country and even from abroad. Admission is
+          made on the basis of merit and is open to students irrespective of
+          caste, creed, religion and nationality.
+        </span>
+      </p>
+
+      <p></p>
+    </div>
+  );
+}
