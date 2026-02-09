d4b2cc7fe2a6609c834348befa76c61418be9a4e
Dev_Srijani <srijani.bluehorse@mail.com>
2026-02-09 17:42:03 +0530

    kjhg

--- components/WelcomeSection.tsx
+++ components/WelcomeSection.tsx
@@ -1,37 +1,39 @@
 const WelcomeSection = () => {
   return (
     <div className="mid_con">
-      <div className="site_title">
-        <h2>Welcome</h2>
-      </div>
+      <div className="container">
+        <div className="site_title">
+          <h2>Welcome</h2>
+        </div>
 
-      <div className="wel_info">
-        <p></p>
-        <p>
-          Apart from the Academic Excellence, West End High School bears the
-          vision of inculcating in the students the right moral values so that
-          they grow up to be able citizens of tomorrow. We make every effort to
-          ensure that our students are global citizens with an Indian heart. As
-          the medium of instruction is English, considerable care and caution is
-          being taken to help the students develop the skills of learning English
-          correctly in real life situations. Perfection is what we seek to
-          import to our children who in their formative stage would feel
-          encouraged to learn with a passion. In this era of careerism and cut
-          throat competition in various professional spheres both communicative
-          and creative English have a wider acceptability in modern times.
-          Perfection is what we seek to import to our children who in their
-          formative stage would feel encouraged to learn with a passion. In this
-          era of careerism and cut throat competition in various professional
-          spheres both communicative and creative English have a wider
-          acceptability in modern times. Perfection is what we seek to import to
-          our children who in their formative stage would feel encouraged to
-          learn with a passion. In this era of careerism and cut throat
-          competition in various professional spheres both communicative and
-          creative English have a wider acceptability in modern times.
-        </p>
-        <p></p>
+        <div className="wel_info">
+          {/* <p></p> */}
+          <p>
+            Apart from the Academic Excellence, West End High School bears the
+            vision of inculcating in the students the right moral values so that
+            they grow up to be able citizens of tomorrow. We make every effort to
+            ensure that our students are global citizens with an Indian heart. As
+            the medium of instruction is English, considerable care and caution is
+            being taken to help the students develop the skills of learning English
+            correctly in real life situations. Perfection is what we seek to
+            import to our children who in their formative stage would feel
+            encouraged to learn with a passion. In this era of careerism and cut
+            throat competition in various professional spheres both communicative
+            and creative English have a wider acceptability in modern times.
+            Perfection is what we seek to import to our children who in their
+            formative stage would feel encouraged to learn with a passion. In this
+            era of careerism and cut throat competition in various professional
+            spheres both communicative and creative English have a wider
+            acceptability in modern times. Perfection is what we seek to import to
+            our children who in their formative stage would feel encouraged to
+            learn with a passion. In this era of careerism and cut throat
+            competition in various professional spheres both communicative and
+            creative English have a wider acceptability in modern times.
+          </p>
+          {/* <p></p> */}
 
-        <div className="orange_line"></div>
+          <div className="orange_line"></div>
+        </div>
       </div>
     </div>
   );
