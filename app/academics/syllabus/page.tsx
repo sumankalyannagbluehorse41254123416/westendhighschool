26e9d0e3ba70d2c9f00492f6dd0cd0826b816099
santana <santanarana3@gmail.com>
2026-02-10 13:10:28 +0530

    change

---
+++ app/academics/syllabus/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import SyllabusBanner from "@/components/academics/syllabus/SyllabusBanner";
+import SyllabusContent from "@/components/academics/syllabus/SyllabusContent";
+
+export default function SyllabusPage() {
+  return (
+    <>
+      <SyllabusBanner />
+      <SyllabusContent />
+    </>
+  );
+}
---
+++ components/academics/AcademicsContent.tsx
@@ -0,0 +1,21 @@
+export default function AcademicsContent() {
+  return (
+    <div className="inner_con">
+      <p>
+        West End School, Jhargram, stands tall as a commemorative edifice of the
+        glorious milieu of the late eighteenth and early nineteenth century
+        Renaissance that swept across Bengal. From the early nineteenth century
+        to the present date, Hindu School has splendidly lived up to its
+        reputation of being a center of academic excellence. A long list of
+        outstanding results in academia right from the year of 1878 till date
+        provides a germane testimony.
+        <br /><br />
+        We have collected these data from the Hindu School Patrika
+        (1986-1989, 1997-1998 &amp; 2000-2001) editions; any omission is strictly
+        involuntary and deeply regretted. Any addition/correction to this data
+        would be highly appreciated and is requested to be communicated to the
+        web team.
+      </p>
+    </div>
+  );
+}
