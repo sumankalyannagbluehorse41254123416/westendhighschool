b6416f28153462d94aff24fa744a8c5c1c297a4e
santana <santanarana3@gmail.com>
2026-02-12 11:38:44 +0530

    change

---
+++ app/infrastructure/transport/page.tsx
@@ -0,0 +1,11 @@
+import TransportBanner from "@/components/infrastructure/transport/TransportBanner";
+import Transport from "@/components/infrastructure/transport/Transport";
+
+export default function TransportPage() {
+  return (
+    <>
+      <TransportBanner />
+      <Transport />
+    </>
+  );
+}
