203382970d8046f12691b0e0620f4e22daa6ca04
santana <santanarana3@gmail.com>
2026-02-12 13:11:26 +0530

    change

---
+++ app/staff/page.tsx
@@ -0,0 +1,10 @@
+import StaffBanner from "@/components/staff/StaffBanner";
+import Team from "@/components/staff/Team";
+
+export default function StaffPage() {
+  return<>
+    <StaffBanner />
+    <Team />      
+    </>;
+}
+
---
+++ components/staff/Team.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Team() {
+  return (
+    <div className="team" style={{ marginBottom: "1%" }}>
+      <ul className="team">
+        <li>
+          <Link href="/staffdetails/14">
+            <Image
+              src="/images/Sir-PA-1.jpg"
+              alt="Pranabananda Barik"
+              width={150}
+              height={180}
+              style={{
+                float: "left",
+                padding: "1%",
+                border: "1px solid #ccc",
+                margin: ".5%",
+                boxShadow: "3px 3px 9px 0px #ccc",
+              }}
+            />
+          </Link>
+
+          <br />
+          <b>Pranabananda Barik</b>
+          <p></p>
+          <p>Secretary</p>
+          <p></p>
+        </li>
+      </ul>
+
+      <div className="clear"></div>
+    </div>
+  );
+}
