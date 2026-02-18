fc5cbd37bbf0b4092c2174178f8c6ff5b8ccd743
Dev_Srijani <srijani.bluehorse@mail.com>
2026-02-18 15:41:15 +0530

    changes

--- components/infrastructure/BusChangePage.tsx
+++ components/infrastructure/BusChangePage.tsx
@@ -1,15 +1,18 @@
 export default function BusChangePage() {
   return (
-    <div className="inner_con">
-      <p>
-        In the case of change in stop and / or bus (of a temporary or permanent
-        nature), the permission for the same has to be sought through an
-        application on prescribed form available at the school Office. The filled
-        in application form along with requisite fee to be submitted at the School
-        Office for permanent change. However the change shall become effective
-        only after a written permission is granted by the school. In such event a
-        new set of ID cards will be issued. (on payment of requisite fees.)
-      </p>
+    <div className="side_padding side_padding_white">
+      <div className="inner_con">
+
+        <p>
+          In the case of change in stop and / or bus (of a temporary or permanent
+          nature), the permission for the same has to be sought through an
+          application on prescribed form available at the school Office. The filled
+          in application form along with requisite fee to be submitted at the School
+          Office for permanent change. However the change shall become effective
+          only after a written permission is granted by the school. In such event a
+          new set of ID cards will be issued. (on payment of requisite fees.)
+        </p>
+      </div>
     </div>
   );
 }
