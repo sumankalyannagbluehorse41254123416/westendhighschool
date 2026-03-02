1cfdd16ad88f1e7f47a029e910550873c6d6f97d
santana <santanarana3@gmail.com>
2026-03-02 17:58:31 +0530

    change

---
+++ app/news_today/[slug]/page.tsx
@@ -0,0 +1,65 @@
+import {NEWS_TODAY_PAGE_IDS } from "@/constant/news_today";
+import { fetchPageData } from "@/services/fetchBlog.service";
+import { headers } from "next/headers";
+import { notFound } from "next/navigation";
+
+interface BlogPost {
+  title?: string;
+  excerpt?: string;
+}
+
+export default async function NewsTodayContain({params}:{
+    params:Promise<{slug:string}>
+}) {
+    const {slug} = await params;
+
+    const id= NEWS_TODAY_PAGE_IDS[slug];
+
+    if(!id){
+        return notFound()
+    }
+
+  const rqHeaders = await headers();
+
+  const host = rqHeaders.get("host") || "localhost:3000";
+  const headersObj = Object.fromEntries(rqHeaders.entries());
+
+  let siteData: any = {};
+
+  try {
+    siteData = await fetchPageData(
+      { host, ...headersObj },
+      id
+    );
+  } catch (error) {
+    console.error("Blog fetch failed");
+    return null;
+  }
+
+  const posts: BlogPost[] = siteData?.userSinglePostdata || [];
+
+  if (!posts.length) {
+    console.warn("No blog data found");
+    return null;
+  }
+
+  // ✅ Your API returns only 1 item
+  const post = posts[0];
+
+  return (
+    <div className="container">
+      <div className="innermiddle">
+        <div className="clear"></div>
+
+        <div className="inner_con general_info_con">
+          <div
+            style={{ textAlign: "justify" }}
+            dangerouslySetInnerHTML={{
+              __html: post?.excerpt || "",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ constant/news_today.ts
@@ -0,0 +1,6 @@
+export const NEWS_TODAY_PAGE_IDS = {
+  "admission-is-going-on-2026-27": "d62a36eb-e9e3-4d58-9035-9fe528788d78",
+  "post-for-assistant-teachers": "77e8ec2a-1996-4bcf-9e54-c976425ebbac",
+  "admission-is-going-on-for-nursery-to-class-11": "d04fd0e9-76e3-4702-9cd9-e87271988038",
+  "time-table-for-annual-examination-2026-27": "82c3064c-4fbb-4a5b-aabd-189e2b9c4f5a",
+} as const;
